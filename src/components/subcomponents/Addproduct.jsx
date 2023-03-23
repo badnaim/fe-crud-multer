0344bbe50cb0dca0bb91b2daf44a3e97270fbe44
badnaim <batnyaam.tumurbat@gmail.com>
2023-03-23 11:53:28 +0800

    little changes

--- src/components/subcomponents/Addproduct.jsx
+++ src/components/subcomponents/Addproduct.jsx
@@ -10,46 +10,86 @@ export default function AddProduct() {
     sale: null,
     brand: "",
     category: "",
-    description: ""
+    description: "",
   });
   // const [prod, setProd] = useState({});
 
   function handleChange(e) {
     setData({ ...data, [e.target.name]: e.target.value });
     console.log(data);
-  };
+  }
 
   function changeHandler(e) {
     console.log(e.target.files[0]);
-    const dataImg = new FormData();
-    data.append("file", e.target.files[0]);
-    // data.append("text", "this is a test mf");
-    setFile(dataImg);
+    setFile(e.target.files[0]);
   }
 
-
-  function uploadHandler() {
+  function uploadHandler(e) {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("data", JSON.stringify(data));
     console.log("uploading");
     fetch("http://localhost:4000/addproduct", {
       method: "POST",
-      body: file,
-      // JSON.stringify({data}),
+      body: formData,
+      // headers: {
+      //   "content-Type":"multipart/form-data"
+      // }
     }).then((res) => console.log(res));
   }
-
-
 
   return (
     <div>
       <form>
-        <input type="text" name="name" placeholder="name" onChange={handleChange} />
-        <input type="number" name="price" placeholder="price" onChange={handleChange} />
-        <input type="number" name="stock" placeholder="stock" onChange={handleChange} />
-        <input type="number" name="sale" placeholder="sale" onChange={handleChange} />
-        <input type="text" name="specs" placeholder="specs" onChange={handleChange} />
-        <input type="text" name="brand" placeholder="brand" onChange={handleChange} />
-        <input type="text" name="category" placeholder="category" onChange={handleChange} />
-        <input type="text" name="description" placeholder="description" onChange={handleChange} />
+        <input
+          type="text"
+          name="name"
+          placeholder="name"
+          onChange={handleChange}
+        />
+        <input
+          type="number"
+          name="price"
+          placeholder="price"
+          onChange={handleChange}
+        />
+        <input
+          type="number"
+          name="stock"
+          placeholder="stock"
+          onChange={handleChange}
+        />
+        <input
+          type="number"
+          name="sale"
+          placeholder="sale"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="specs"
+          placeholder="specs"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="brand"
+          placeholder="brand"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="category"
+          placeholder="category"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="description"
+          placeholder="description"
+          onChange={handleChange}
+        />
         <br />
         <label>file</label>
         <br />
