38838907035aee86322ad6872eb453ded3064c1b
badnaim <batnyaam.tumurbat@gmail.com>
2023-03-23 08:05:24 +0800

    march 23

--- src/components/subcomponents/Addproduct.jsx
+++ src/components/subcomponents/Addproduct.jsx
@@ -3,43 +3,59 @@ import { useState } from "react";
 
 export default function AddProduct() {
   const [file, setFile] = useState();
+  const [data, setData] = useState({
+    name: "",
+    price: null,
+    stock: null,
+    sale: null,
+    brand: "",
+    category: "",
+    description: ""
+  });
+  // const [prod, setProd] = useState({});
+
+  function handleChange(e) {
+    setData({ ...data, [e.target.name]: e.target.value });
+    console.log(data);
+  };
+
+  function changeHandler(e) {
+    console.log(e.target.files[0]);
+    const dataImg = new FormData();
+    data.append("file", e.target.files[0]);
+    // data.append("text", "this is a test mf");
+    setFile(dataImg);
+  }
+
 
   function uploadHandler() {
     console.log("uploading");
     fetch("http://localhost:4000/addproduct", {
       method: "POST",
       body: file,
+      // JSON.stringify({data}),
     }).then((res) => console.log(res));
   }
 
-  function changeHandler(e) {
-    console.log(e.target.files[0]);
-    const data = new FormData();
-    data.append("file", e.target.files[0]);
-    // data.append("text", "this is a test mf");
-    setFile(data);
-  }
-  // console.log(setFile);
+
 
   return (
     <div>
       <form>
-        {/* <label>name</label>
-        <input type="text" />
-        <label>price</label>
-        <input type="number" />
-        <label>stock</label>
-        <input type="number" />
-        <label>specs</label>
-        <input type="text" /> */}
+        <input type="text" name="name" placeholder="name" onChange={handleChange} />
+        <input type="number" name="price" placeholder="price" onChange={handleChange} />
+        <input type="number" name="stock" placeholder="stock" onChange={handleChange} />
+        <input type="number" name="sale" placeholder="sale" onChange={handleChange} />
+        <input type="text" name="specs" placeholder="specs" onChange={handleChange} />
+        <input type="text" name="brand" placeholder="brand" onChange={handleChange} />
+        <input type="text" name="category" placeholder="category" onChange={handleChange} />
+        <input type="text" name="description" placeholder="description" onChange={handleChange} />
+        <br />
         <label>file</label>
+        <br />
         <input type="file" onChange={changeHandler} />
-        <input type="button" onClick={uploadHandler} value={"Upload"} />
+        <input type="submit" onClick={uploadHandler} value={"Upload"} />
       </form>
-      {/* <input type="number" value={"sale"} /> */}
-      {/* <input type="text" value={"brand"} />
-      <input type="text" value={"category"} />
-      <input type="text" value={"description"} /> */}
     </div>
   );
 }
