b1b0477805de0872ac549df52cbe6ad7c638dadd
badnaim <batnyaam.tumurbat@gmail.com>
2023-03-27 08:09:42 +0800

    get in dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,15 @@
 import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 // import "../styles/dashboard.css";
 
 export default function Dashboard() {
+  const [products, setProducts] = useState();
+  useEffect(() => {
+    axios.get("http://localhost:4000/get").then((res) => setProducts(res.data));
+    console.log(setProducts);
+  }, []);
   return (
-    <div>dashboard</div>
+    <div>{products}</div>
   );
 }
