fbabd028836a52f5d5ca42ecbf8795a4e4f1193f
badnaim <batnyaam.tumurbat@gmail.com>
2023-03-30 08:13:18 +0800

    category add

--- src/components/subcomponents/AddCategory.jsx
+++ src/components/subcomponents/AddCategory.jsx
@@ -1,39 +1,52 @@
-import react, { useState } from "react";
+import { useState } from "react";
 import "../../styles/addCategory.css";
 
 export default function AddCategory() {
+  const [categoryImage, setCategoryImage] = useState();
   const [categoryName, setCategoryName] = useState({
     name: ""
   });
-  const [categoryImage, setCategoryImage] = useState();
 
   function nameHandler(e) {
     setCategoryName({ [e.target.name]: e.target.value });
-    console.log(categoryName);
   }
+  console.log(categoryName);
 
   function imageHandler(e) {
     console.log(e.target.files[0]);
     setCategoryImage(e.target.files[0]);
   }
+  // console.log("categ img", categoryImage);
 
-  function categoryUploader(e) {
-    e.preventDefault();
-    const formData = new FormData();
-    formData.append("image", categoryImage);
-    formData.append("name", JSON.stringify(categoryName));
-    console.log(formData);
+  console.log("ghj", JSON.stringify(categoryName));
+
+  async function categoryUploader(e) {
+    // e.preventDefault();
+    const categData = new FormData();
+    categData.append("file", categoryImage);
+    categData.append("name", JSON.stringify(categoryName));
+    console.log("uploading");
     fetch("http://localhost:4000/addCategory", {
       method: "POST",
-      body: formData,
-    }).then((res) => console.log(res));
+      body: categData,
+    }).then((res) => {
+      console.log(res, "res");
+    });
+    // console.log("formdata", formData);
+    // fetch("http://localhost:4000/addCategory", {
+    // method: "POST",
+    // body: formData,
+    // }).then((res) => console.log(res));
   }
+
 
   return (
     <div className="addCategory">
-      <input type="text" placeholder="category name" onChange={nameHandler} />
-      <input type="file" placeholder="category image" onChange={imageHandler} />
-      <button onClick={categoryUploader}>Add category</button>
+      <form>
+        <input name="name" type="text" placeholder="category name" onChange={nameHandler} />
+        <input type="file" placeholder="category image" onChange={imageHandler} />
+        <input type="submit" value={"Create category"} onClick={categoryUploader} />
+      </form>
     </div>
   );
 }
